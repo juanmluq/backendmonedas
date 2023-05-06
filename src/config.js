07fb86ddbe5999f0b9c42743be19107359cfdc26
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2023-05-06 20:16:38 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const DB_PORT = process.env.PORT || 3001;
 const DB_USER= process.env.DB_USER || "postgres";
 const DB_PASSWORD = process.env.DB_PASSWORD || "11QAZWSX";
-const DB_HOST = process.env.DB_HOST || "127.0.0.23";
+const DB_HOST = process.env.DB_HOST || "localhost";
 const DB_NAME = process.env.DB_NAME || "usersmoneda";
 
 module.exports={
