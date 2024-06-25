21e300c72f6a3752c6d978e8fad744bd98e3ef69
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2024-06-25 20:46:56 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -8,7 +8,7 @@ const {
 // `postgresql://postgres:pbd1jHFIBof7LqPaQBcN@containers-us-west-209.railway.app:7122/railway`
 // `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 // `postgres://postgres:11QAZWSX@localhost/usersmoneda`
-const sequelize = new Sequelize(`postgresql://postgres:pbd1jHFIBof7LqPaQBcN@containers-us-west-209.railway.app:7122/railway`
+const sequelize = new Sequelize(`postgresql://postgres:ADdDZBnrtFcVvVINthmtSLbFmTcAsHdS@roundhouse.proxy.rlwy.net:18001/railway`
 , {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
