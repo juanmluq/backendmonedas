fa7080a473161ab4d5265fe559f9904dc3fe421c
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2025-03-31 22:51:45 -0300

    Update Usermoneda.js

--- src/models/Usermoneda.js
+++ src/models/Usermoneda.js
@@ -1,19 +1,15 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
   sequelize.define('usermoneda', {
     id: {
-      type: DataTypes.UUID, //es un numero random con numeros y letras que no se va a repetir. Porq cuando mando los datos de la api asi no se van a pisar los id
+      type: DataTypes.UUID, 
       defaultValue: DataTypes.UUIDV4, //
-      allowNull: false, //allownull dice que no te permito que no este en vacio. si le pongo false le digo que no este vacio.
-      primaryKey: true //es la clave primaria osea el id.
+      allowNull: false,
+      primaryKey: true
     },
     name: {
       type: DataTypes.STRING(50),
       allowNull: false,
-      //unique: esto me sirve para un mail por ejemplo. suponiendo que registro mi mail luque11@gmail.com si viene otro a querer regristrarse con el mismo mail no lo deja
     },
     user: {
       type: DataTypes.STRING(25),
