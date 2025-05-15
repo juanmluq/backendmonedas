9844645757a78bcedca5443356eb4db32e7855fc
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2025-05-15 20:38:56 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 //const DB_PORT = process.env.PORT || 5432;
-const DB_PORT = 5432;
+const DB_PORT = 8080;
 const DB_USER= process.env.DB_USER || "postgres";
 const DB_PASSWORD = process.env.DB_PASSWORD || "11QAZWSX";
 const DB_HOST = "localhost";
